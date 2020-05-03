96b0ac810d49f29adce2e686d12de80fd3c7e60b
Garrett Sutula <garrettsutula@gmail.com>
2020-05-03 14:28:11 -0400

    Angular, dependency update to fix vulns

--- src/app/ng-material/ng-material.module.ts
+++ src/app/ng-material/ng-material.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatToolbarModule, MatExpansionModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @NgModule({
   imports: [MatButtonModule, MatToolbarModule, MatExpansionModule],
